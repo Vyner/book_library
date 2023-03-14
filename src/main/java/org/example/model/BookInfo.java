d21eddeea3c3a7ad0f2565cc9aa2cc14146c8634
vyner <vynercylma@outlook.com>
2023-03-14 20:53:28 +0800

    add BookInfo model

--- src/main/java/org/example/model/BookInfo.java
+++ src/main/java/org/example/model/BookInfo.java
@@ -1,4 +1,10 @@
 package org.example.model;
 
 public class BookInfo {
+
+    private String name;
+    private Float price;
+
+    private String auther;
+
 }
---
+++ src/test/java/org/example/service/BookServiceTest.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+class BookServiceTest {
+
+    @Test
+    void store() {
+
+    }
+}
